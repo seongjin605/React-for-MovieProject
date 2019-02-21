2db02883fed38084e685f1217a4e5ce2e3e07114
seongjin605 <qkrtjdwls605@naver.com>
2019-02-21 19:49:47 +0900

    Updates

---
+++ precache-manifest.d2bd0c9cdb2282f756b9eb9c8131a96c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "23f3bedd7c5eb78545fd",
+    "url": "/movie_app/static/js/main.23f3bedd.chunk.js"
+  },
+  {
+    "revision": "5178fb6fe39fb9e436b0",
+    "url": "/movie_app/static/js/2.5178fb6f.chunk.js"
+  },
+  {
+    "revision": "23f3bedd7c5eb78545fd",
+    "url": "/movie_app/static/css/main.5a5894a0.chunk.css"
+  },
+  {
+    "revision": "20a7155f3d046126edc05c101dc4de94",
+    "url": "/movie_app/index.html"
+  }
+];
